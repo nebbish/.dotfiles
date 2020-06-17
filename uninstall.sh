ab2f48ac499cd1d8e0f5961c3e7a63dccd5331ab
nebbish <nebbish@12esults.com>
2020-06-16 20:58:11 -0700

    Updating bootstrap/uninstall with new 'packages'
    The loop that stows & unstows is still hard coded, and needed updating
    after the refactor to handle the new folders 'allshells' and 'zshell'

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for d in bash bin_files devstuff git p4 screen vim; do
+for d in allshells bash bin_files devstuff git p4 screen vim zshell; do
 	echo "Unstowing $d ..."
 	stow -D $d
 done
