6ce85cdcbf76354edbdff7477a3e42876ed64f72
nebbish <nebbish@12esults.com>
2020-07-01 17:50:06 -0700

    Scattered minor tweaks (alias,ignore,screen,...)
    Adding 'rmemptydirs' even for mac
    Changing mode of a testing 'py' script
    Adding tip for sh script to get path of active script
    Changing the status line of my 'screen's
    Updating the Zsh prompt to work with screen's dynamic titles - but it is
    not working

--- private/endot.sh
+++ private/endot.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+##
+## NOTE:  I just got the idea that this could be encrypting a private GIT archive
+##        that ONLY lives in a sub-dir  that is ignored from the ".dotfiles" repo
+##
+##        then I looked up a 'bare' repo, seems what i want.
+##        * To make the initial repo (only once):
+##                `git init --bare ~/.dotfiles/private/private.git`
+##        * then the 'endot' flow will be:
+##            - tar & encrypt the 'bare' archive: ~/.dotfiles/private/private.git.tgz.gpg
+##        * and 'dedot' will involve a bit more:
+##            - decrypt & untar the 'bare' archive
+##            - if local archive exists (~/.dotfiles/private/.git)
+##            -     maybe run a 'pull' from the freshly decrypted, maybe updated, shared repo
+##            - else
+##            -     `git clone` the shared 'bare' repo into this folder as a local repo
+##            -     (kinda funny since it is a sub-folder - but it works)
+##
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $dir >/dev/null
 tar -czf local.tgz .local/
