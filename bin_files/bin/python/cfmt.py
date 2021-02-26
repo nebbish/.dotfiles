c96938e5db48afabb762b065464e1611f9a9f0d6
nebbish <nebbish@12esults.com>
2021-02-25 23:35:19 -0800

    Adding a 'cfmt' PY wrapper for clang-format
    This allows me to more invoke the external tool from within VIM with the
    same ease I have on the command line:   without having to specify
    '-style=file' every time.
    
    Additionally -- this script also silently attempts to P4 checkout the
    file if one was specified.  Can be used with stdout, so there may not
    be a file to edit.

---
+++ bin_files/bin/python/cfmt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+import os, sys
+
+sys.dont_write_bytecode = True
+from where import where_impl
+
+def main():
+	tool = where_impl('clang-format')[0]
+
+	if len(sys.argv) == 1:
+		os.execv(tool, ['-style=file'])
+	
+	# This returns a file-like object whose .close() method returns the
+	# exit code of the child process....   we ignore it for this pre-edit
+	os.popen('p4 edit "{}" 2>/dev/null'.format(sys.argv[1])).close()
+
+	os.execv(tool, ['-style=file', '-i', sys.argv[1]])
+
+
+if __name__ == '__main__':
+	sys.exit(main())
+
--- bin_files/bin/python/where.py
+++ bin_files/bin/python/where.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python2
 import os, sys, platform, argparse
 
-in_win = platform.system().lower().startswith('win')
+def where_impl(val):
+	found = []
+	exts = ['']
+	in_win = platform.system().lower().startswith('win')
+	if in_win: exts += os.environ['PATHEXT'].split(os.pathsep)
+	for p in os.environ['PATH'].split(os.pathsep):
+		for e in exts:
+			path = os.path.join(p, val + e)
+			if os.path.exists(path):
+				found.append(path)
+	return found
+
+
 def main():
 	parser = argparse.ArgumentParser(description="List each occurance of the provided command in the current PATH")
 	parser.add_argument("expr", help = 'Specify the expression to search for')
 	cargs = parser.parse_args()
 
-	found_one = False
-	exts = ['']
-	if in_win: exts += os.environ['PATHEXT'].split(os.pathsep)
-	for p in os.environ['PATH'].split(os.pathsep):
-		for e in exts:
-			path = os.path.join(p, cargs.expr + e)
-			if os.path.exists(path):
-				print(path)
-				found_one = True
-	return 0 if found_one else 1
+	res = where_impl(cargs.expr)
+	for p in res:
+		print(p)
+	return 0 if len(res) else 1
 
 if __name__ == '__main__':
 	sys.exit(main())
