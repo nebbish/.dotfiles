a24762f130e276fa64c29f5e6c2cbb39db489513
nebbish <nebbish@12esults.com>
2022-10-14 15:25:00 -0700

    encrypt/decrypt: scripts now honor output option
    This was added b/c now I need to use Ramdisks to remain secure, due to
    the inability to erase data from an SSD.
    
    So now the encrypted file can remain living on the SSD, while the
    decrypted data will be written directly to the "Ramdisk" volume.
    (and vice-versa for encryption)

--- bin_files/bin/shell/decrypt.sh
+++ bin_files/bin/shell/decrypt.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-dest=${1%.gpg}
+if [[ -n $2 ]]; then
+    dest=$2
+elif [[ ${1%.gpg} != $1 ]]; then
+    dest=${1%.gpg}
+elif [[ ${1%.pgp} != $1 ]]; then
+    dest=${1%.pgp}
+else
+    dest=$1.out
+fi
+
 gpg -o $dest -d $1
 
--- bin_files/bin/shell/encrypt.sh
+++ bin_files/bin/shell/encrypt.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-dest=${1}.gpg
+if [[ -n $2 ]]; then
+    dest=$2
+else
+    dest=${1}.gpg
+fi
+
 gpg --cipher-algo aes256 -c -o $dest $1
 
