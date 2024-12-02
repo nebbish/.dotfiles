640d62523646e8567618a6c22e3d7a4dddae5bf9
nebbish <nebbish@12esults.com>
2024-12-02 12:38:13 -0800

    fixing stow ignoring: adding a global stow ignore
    This change involves tweaking the 'bootstrap' script to FIRST "stow" the
    files that will control `stow`:  the global ignore file.
    
    Then subsequent launches of `stow` will have a global ignore to ensure
    no temp files get installed
    
    (my 'git' package still has a local ignore, for special ignoring)

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,34 @@
 ##         * install nux-dextop repo (see: https://unix.stackexchange.com/a/395827/194787)
 ##
 
-for d in allshells bash bin_files config devstuff git p4 screen vim zshell; do
+##
+## Stow the "stow_files" package first -- it sets up the 'global stow ignore'
+##
+for d in stow_files; do
 	echo "Stowing $d ..."
 	stow -R $d "$@"
+done
+
+
+##
+## Now stow all the packages that can be folded
+##
+for d in allshells bash bin_files devstuff git p4 screen zshell; do
+	echo "Stowing $d ..."
+	stow -R $d "$@"
+done
+
+##
+## Now the packages that should NOT be folded
+##
+##    vim/.vim:
+##        it is typically created and maintained by VIM upon first launch
+##
+##    config/.config: 
+##        a few tools expect to drop files here
+##
+for d in config vim; do
+	echo "Stowing $d ..."
+	stow --no-folding -R $d "$@"
 done
 
--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,12 @@ for d in allshells bash bin_files config devstuff git p4 screen vim zshell; do
 	stow -D $d
 done
 
+##
+## Unstow this last -- so the 'global stow ignore' is in place for all of the above
+## (not sure if this is necessary)
+##
+for d in stow_files; do
+	echo "Unstowing $d ..."
+	stow -D $d
+done
+
