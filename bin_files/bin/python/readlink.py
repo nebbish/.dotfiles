fb152d759b9a4a7f76436057d398927ffd0f034e
nebbish <nebbish@12esults.com>
2024-12-14 14:58:26 -0800

    adding `pyenv` setup commands in my allshells files
    The commands only run if `pyenv` command is available on the path.
    
    Also this change required fixes to my own `readlink.py` script which
    over-rides the built-in -- so it behaves like the built-in.
    
    If I 'vaguely' recall correctly, my original motivation to create a
    'readlink' PY script was because the basic tool is not present by
    default on Windows OS's (and now that I write this, I wonder why I
    didn't try to update my Cygwin install to include it...  hmmm)
    
    So... deciding to keep it around, I adjusted it so that the original
    code use only used when on Windows OS's, and when on NON-Windows OS it
    behaves like the built-in command
    
    NOTE:  it took me many hours to debug why my `pyenv init` command was
    hanging and not working, when all along it was because of my "broken?"
    readlink.py script.  The `--debug` cmd-line option to pyenv was the
    trick to track it down.

--- bin_files/bin/python/readlink.py
+++ bin_files/bin/python/readlink.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
-import os, sys, argparse
+import os, sys, argparse, platform
 parser = argparse.ArgumentParser(description="python's readlink() as a command")
 parser.add_argument("path", default=None, nargs='?',
 					help="path to pass into os.path.realpath()")
 parser.add_argument("-f", "--canonicalize", action="store_true",
 					help="Ignored by this script")
 cargs, _ = parser.parse_known_args()
-print(os.path.realpath(cargs.path))
+
+if platform.system().lower().startswith('win'):
+	# For windows we use realpath:
+	print(os.path.realpath(cargs.path))
+else:
+	# For unix, we use readlink - so we also have to catch errors
+	try:
+		print(os.readlink(cargs.path))
+	except OSError:
+		sys.exit(1) # print nothing, but exit with a '1'
